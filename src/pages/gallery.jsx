c3b7ba15f1ae568c72344440f07d0b982aaffa7d
kaynekaynekayne <theygoon@gmail.com>
2022-12-08 01:04:45 +0900

    change position

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -10,27 +10,14 @@ const Gallery = () => {
     return (
         <Container>
             <div className="left">
-                <h4>sub</h4>
-                <div className='item-2x'>
-                    {/* <h1>PRO</h1>
-                    <h1>JECTS</h1> */}
-                </div>
-                <div className='item'>
-                    <Link to="/my-words">
-                        <img src={mywords} alt="dictionary"/>
-                        <h5>My Words</h5>
-                    </Link>
-                </div>
-            </div>
-            <div className="right">
-                <h4>main</h4>
-                <div className="item">
+                {/* <h4>main</h4> */}
+                <div className="item item-main">
                     <Link to="/around">
                         <img src={map} alt="theater-map"/>
                         <h5>Around</h5>
                     </Link>
                 </div>
-                <div className="item">
+                <div className="item item-main">
                     <Link to="/travel-call">
                         <img src={emergency} alt="emergency-call"/>
                         <h5>Travel Emergency Call</h5>
@@ -43,6 +30,21 @@ const Gallery = () => {
                     </Link>
                 </div>
             </div>
+            <div className="right">
+                {/* <h4>sub</h4> */}
+                <div className='item'>
+                
+                </div>
+                <div className='item'>
+                
+                </div>
+                <div className='item'>
+                    <Link to="/my-words">
+                        <img src={mywords} alt="dictionary"/>
+                        <h5>My Words</h5>
+                    </Link>
+                </div>
+            </div>
         </Container>
     )
 };
@@ -52,16 +54,16 @@ const Container=styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align:center;
     
-    .item-2x{
+    /* .item-2x{
         width:11rem;
         height:23rem;
         margin:1rem 0.5rem;
         border-radius: 1rem;
         border:3px solid white;
         color:whitesmoke;
-        /* background-color: #272727; */
-    }
+    } */
 
     .item{
         width:11rem;
@@ -86,6 +88,11 @@ const Container=styled.div`
                 transform: scale(1.13);
             }
         }
+    }
+
+    .item-main{
+        border:5px solid white;
+
     }
 `;
 
